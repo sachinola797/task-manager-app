3ded845d67265598a05d87d9c4a45dba0ab03cbc
Sachin Olla <sachin.olla@affle.com>
2022-02-20 20:14:03 +0530

    Implemented Resource Updating & Deleting Endpoints

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ app.get('/users/:id', async (req, res) => {
         const user = await User.findById(req.params.id);
         console.log(user);
         if (!user) {
-            return res.status(400).send("User doesn't exist");
+            return res.status(404).send("User doesn't exist");
         }
         res.status(200).send(user);
     } catch (err) {
@@ -40,6 +40,52 @@ app.get('/users', async (req, res) => {
     try {
         const users = await User.find({});
         res.status(200).send(users);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+// update single user by id
+app.patch('/users/update/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["name", "password", "age"];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+    if(!isValidOperation) {
+        return res.status(400).send({errName: "Invalid upates!", errorMessage: "Trying to update invalid or non-updateable properties"})
+    }
+    
+    try {
+        const user = await User.findOneAndUpdate({
+                    _id: req.params.id
+                }, req.body,
+                { 
+                    upsert: false,
+                    runValidators: true, 
+                    returnDocument: 'after'
+                }
+            );
+        if (!user) {
+            return res.status(404).send("User doesn't exist");
+        }
+        res.status(200).send(user);
+    } catch (err) {
+        res.status(400).send({
+            errorName: err.name,
+            errorMessage: err.message,
+        });
+    }
+});
+
+
+// delete single user
+app.delete('/users/delete/:id', async (req, res) => {
+    try {
+        const user = await User.findOneAndDelete({_id: req.params.id});
+        if (!user) {
+            return res.status(404).send("User doesn't exist");
+        }
+        res.status(200).send("User deleted successfully!");
     } catch (err) {
         res.status(500).send(err);
     }
@@ -64,7 +110,7 @@ app.get('/tasks/:id', async (req, res) => {
         const task = await Task.findById(req.params.id);
         console.log(task);
         if (!task) {
-            return res.status(400).send("Task doesn't exist");
+            return res.status(404).send("Task doesn't exist");
         }
         res.status(200).send(task);
     } catch (err) {
@@ -77,6 +123,51 @@ app.get('/tasks', async (req, res) => {
     try {
         const tasks = await Task.find({});
         res.status(200).send(tasks);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+// update single task by id
+app.patch('/tasks/update/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["title", "description", "completed"];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+    if(!isValidOperation) {
+        return res.status(400).send({errName: "Invalid upates!", errorMessage: "Trying to update invalid or non-updateable properties"})
+    }
+    
+    try {
+        const task = await Task.findOneAndUpdate({
+                    _id: req.params.id
+                }, req.body,
+                { 
+                    upsert: false,
+                    runValidators: true, 
+                    returnDocument: 'after'
+                }
+            );
+        if (!task) {
+            return res.status(404).send("Task doesn't exist");
+        }
+        res.status(200).send(task);
+    } catch (err) {
+        res.status(400).send({
+            errorName: err.name,
+            errorMessage: err.message,
+        });
+    }
+});
+
+// delete single task
+app.delete('/tasks/delete/:id', async (req, res) => {
+    try {
+        const task = await Task.findOneAndDelete({_id: req.params.id});
+        if (!task) {
+            return res.status(404).send("Task doesn't exist");
+        }
+        res.status(200).send("Task deleted successfully!");
     } catch (err) {
         res.status(500).send(err);
     }
