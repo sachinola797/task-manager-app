4368f95326f26c6c4b106573008f93fe0b2cc1a6
Sachin Olla <sachin.olla@affle.com>
2022-03-04 00:29:29 +0530

    minor changes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,29 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-mongoose.connect(process.env.DB_URL, {
+mongoose.connect(process.env.MONGODB_REMOTE_URL, {
     useNewUrlParser: true,
     // useCreateIndex: true
 });
-
-// const me = new User({
-//     name: 'Sachin Olla',
-//     email: 'sachin@gmail.com',
-//     password: ' ad$gsdgsd    ',
-//     age: 45,
-// })
-
-// me.save().then(result => {
-//     console.log(result);
-// }).catch(err => {
-//     console.log(err);
-// })
-
-// Task.create({
-//     description: '  Buy a tester     ',
-//     // completed: false
-// }).then(result => {
-//     console.log(result);
-// }).catch(err => {
-//     console.log(err);
-// })
